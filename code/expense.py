0eba802637b4eec505969cf6a0ab1b16fd0f2b36
shrishty19 <1912ssingh@gmail.com>
2023-10-17 23:50:10 +0530

    category validations and selection

--- code/expense.py
+++ code/expense.py
@@ -11,14 +11,14 @@ def process_expense_command(message, bot):
     msg = bot.send_message(message.chat.id, "Choose an option:", reply_markup=markup)
     bot.register_next_step_handler(msg, expense_option_selection, bot)
 
-    def expense_option_selection(message, bot):
+ def expense_option_selection(message, bot):
     selected_option = message.text
     if selected_option == "Add":
         select_expense_category(message, bot)
     elif selected_option == "Delete":
         delete_expense(message, bot)  # Call the delete_expense function.
     elif selected_option == "Update":
-        edit.run(message, bot)  # This calls the edit functionality
+        edit.run(message, sbot)  # This calls the edit functionality
     else:
         bot.send_message(message.chat.id, "Invalid option. Please try again.")
 
@@ -29,3 +29,17 @@ def select_expense_category(message, bot):
         markup.add(c)
     msg = bot.send_message(message.chat.id, "Select Category for expense:", reply_markup=markup)
     bot.register_next_step_handler(msg, expense_category_selected, bot)
+
+def expense_category_selected(message, bot):
+    try:
+        chat_id = message.chat.id
+        selected_category = message.text
+        if selected_category not in helper.getSpendCategories():
+            bot.send_message(chat_id, "Invalid", reply_markup=telebot.types.ReplyKeyboardRemove())
+            raise Exception(f'Sorry, I don\'t recognize this category "{selected_category}"!')
+
+        markup = telebot.types.ReplyKeyboardRemove()
+        msg = bot.send_message(chat_id, f"How much did you spend on {selected_category}?", reply_markup=markup)
+        bot.register_next_step_handler(msg, record_expense, selected_category, bot)
+    except Exception as e:
+        bot.reply_to(message, "Oh no! " + str(e))
