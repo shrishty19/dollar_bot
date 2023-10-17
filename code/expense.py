1477e368a28d5b350f7d49c590503fb7fba976ac
shrishty19 <1912ssingh@gmail.com>
2023-10-17 23:48:41 +0530

    Validation checks

--- code/expense.py
+++ code/expense.py
@@ -10,3 +10,14 @@ def process_expense_command(message, bot):
         markup.add(opt)
     msg = bot.send_message(message.chat.id, "Choose an option:", reply_markup=markup)
     bot.register_next_step_handler(msg, expense_option_selection, bot)
+
+    def expense_option_selection(message, bot):
+    selected_option = message.text
+    if selected_option == "Add":
+        select_expense_category(message, bot)
+    elif selected_option == "Delete":
+        delete_expense(message, bot)  # Call the delete_expense function.
+    elif selected_option == "Update":
+        edit.run(message, bot)  # This calls the edit functionality
+    else:
+        bot.send_message(message.chat.id, "Invalid option. Please try again.")
